6d7205b2dc594bd002bfd9af63611c88551bf602
alex1 <nuorama@gmail.com>
2014-05-24 00:20:29 +0300

    Back-up on mini-laptop.

--- posix/client-server-shared-memory/client.c
+++ posix/client-server-shared-memory/client.c
@@ -11,6 +11,7 @@
 #include <netdb.h>
 #include <string.h>
 
+#include <sys/mman.h>
 
 
 #include "common.h"
@@ -18,7 +19,7 @@
 #define NO_OF_REQUESTS 5
 #define REPLY_TIMEOUT 1 //30 seconds
 
-int doSocketOperations(int client_id) {
+int doSocketOperations(int client_id, void* mapped_mem) {
 
 
     //prepare buffer for writing request
@@ -114,7 +115,9 @@ int doSocketOperations(int client_id) {
     char reply_buf[OUT_BUFFER_SIZE];
     time_t start_time = time(NULL);
     time_t elapsed_time = 0;
-    while ((elapsed_time <= REPLY_TIMEOUT) && (read_bytes < IN_BUFFER_SIZE)) {
+    int spin_count = -1;
+    while ((elapsed_time <= REPLY_TIMEOUT) && (read_bytes < OUT_BUFFER_SIZE)) {
+        spin_count += 1;
         int nfds = epoll_wait(epoll_fd, events, 10, REPLY_TIMEOUT);
         if (nfds == -1) {
             printf("doSocketOperations - epoll_wait failed\n");
@@ -144,20 +147,38 @@ int doSocketOperations(int client_id) {
         elapsed_time = crt_time - start_time;        
     }
     if (read_bytes == 0)
-        printf("doSocketOperations - client_id=%d, nothing received on socket\n", client_id);
+        printf("doSocketOperations - client_id=%d, nothing received on socket, spin_count=%d\n", client_id, spin_count);
     if (read_bytes < OUT_BUFFER_SIZE) {
-        printf("doSocketOperations - client_id=%d, read only %d bytes\n", client_id, read_bytes);
-        printf("doSocketOperations - client_id=%d exiting\n", client_id);
+        printf("doSocketOperations - client_id=%d, read only %d bytes, spin_count=%d - exiting\n", client_id, read_bytes, spin_count);
         return -4;
     }
     else {
-        printf("doSocketOperation - client_id=%d, read enough bytes, proceding to process them\n", client_id);
+        printf("doSocketOperation - client_id=%d, read %d bytes, spin_count=%d, proceding to process reply\n", client_id, read_bytes, spin_count);
     }
 
     //if everything worked, we can now interpret the buffer
+    double reply_val;
+    int reply_thread;
+    int buf_index = 0;
+    memcpy(&reply_val, reply_buf+buf_index, sizeof(reply_val)); buf_index += sizeof(reply_val);
+    memcpy(&reply_thread, reply_buf+buf_index, sizeof(reply_thread)); buf_index += sizeof(reply_thread);
 
+    printf("doSocketOperations - client_id=%d, value=%g, op=%d, reply_val=%g, reply_thread=%d\n",
+           client_id, value, op, reply_val, reply_thread);
 
-    printf("doSocketOperations exiting\n");
+    
+    int write_offset = MMAP_HEADER_SIZE + client_id * MMAP_ITEM_SIZE;
+    void *write_address = mapped_mem + write_offset;
+    write_offset = 0;
+    memset(write_address + write_offset, 0, sizeof(int)); write_offset += sizeof(int);
+    memcpy(write_address + write_offset, &client_id, sizeof(client_id)); write_offset += sizeof(client_id);
+    memcpy(write_address + write_offset, &reply_thread, sizeof(reply_thread)); write_offset += sizeof(reply_thread);
+    memcpy(write_address + write_offset, &op, sizeof(op)); write_offset += sizeof(op);
+    memcpy(write_address + write_offset, &value, sizeof(value)); write_offset += sizeof(value);
+    memcpy(write_address + write_offset, &reply_val, sizeof(reply_val)); write_offset += sizeof(reply_val);
+    memset(write_address + write_offset, 0, 2 * sizeof(struct timespec)); write_offset += 2*sizeof(struct timespec);
+
+    printf("doSocketOperations exiting, wrote result at %p\n", write_address);
     return 0;
 }
 
@@ -165,6 +186,7 @@ int doSocketOperations(int client_id) {
 int main(int argc, char* argv[]) {
     int pids[NO_OF_REQUESTS];
 
+    void* mapped_mem = mmap(NULL, MMAP_HEADER_SIZE + NO_OF_REQUESTS * MMAP_ITEM_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
 
     for (int i = 0; i < NO_OF_REQUESTS; i++) {
         //spawn a new child
@@ -173,17 +195,22 @@ int main(int argc, char* argv[]) {
             pids[i] = pid;
         }
         else {
-            return doSocketOperations(i);
+            return doSocketOperations(i, mapped_mem);
             //open socket, write to it and read from it, then close; write to shared memory that we have finished
         }
         
     }
 
     int status;
-    for (int i = 0; i < NO_OF_REQUESTS; i++) {
-        int ret = waitpid(pids[i], &status, WUNTRACED|WEXITED|WCONTINUED);
-        printf("Child:%d, pid:%d, ret=%d\n", i, pids[i], ret);
+    int stopped_children = 0;
+    while (stopped_children < NO_OF_REQUESTS) {
+        int ret = waitpid(-1, &status, 0);
+        stopped_children += 1;
+        printf("Child pid:%d, status=%d, total finished=%d\n", ret, status, stopped_children);
     }
+    
+    
+
     printf("Program exiting\n");
     return 0;
 }
