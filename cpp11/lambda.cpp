53c8e0f854c9c8b580215924a7601118bd2715d4
U-GAMELOFT\petrisoralexandru.d <petrisoralexandru.d@bucwks0777.buc.gameloft.org>
2014-08-29 20:10:04 +0300

    eod backup.

--- cpp11/lambda.cpp
+++ cpp11/lambda.cpp
@@ -1,4 +1,25 @@
 #include <stdio.h>
+#include <iostream>
+
+auto LCall() {
+    return [] () { printf("lambda from LCall\n"); };
+}
+
+auto LCall2() {
+    [] () { printf("lambda from LCall2\n"); }; //does nothing but define that lambda function, does not call or return it in any way
+}
+
+auto LCall3() {
+    [] () { printf("lambda from LCall3\n"); }();
+}
+
+auto LCall4() {
+    [] () { printf("lambda from LCall4, ret=13\n"); return 13; } (); //function call with no effect
+}
+
+auto LCall5() {
+    return [] () { printf("lambda from LCall4, ret=13\n"); return 13; }();  //returns the value from the function call
+}
 
 int main() {
 
@@ -8,4 +29,28 @@ int main() {
     auto func2 = [] () {  printf("inside anonymous func2\n"); };
     func2();
     func2();
+
+    [] () { printf("inside anonymous - 3\n"); }();
+
+    auto func3 = LCall();
+    printf("func3: ");
+    func3();
+    
+    printf("LCall(): ");
+    LCall();
+    printf("\nLCall() - 2: ");
+    LCall()();
+
+    printf("\nLCall2(): ");
+    LCall2();
+
+    printf("\nLCall3(): ");
+    LCall3();
+
+    //auto retVal4 = LCall4();  //compile error-incomplete type
+    //std::cout << "LCall4 ret=" << retVal4;
+
+    auto retVal5 = LCall5();
+    std::cout << "LCall5 ret=" << retVal5;  
+    
 }
