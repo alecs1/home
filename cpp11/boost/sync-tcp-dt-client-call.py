c2aab2e35b9addca93c960f2bfc0ba1b85034113
U-GAMELOFT\petrisoralexandru.d <petrisoralexandru.d@bucwks0777.buc.gameloft.org>
2014-09-09 19:23:00 +0300

    Really started work on the distributed program. echo-server-stackful-coroutines.cpp and example-server.cpp (aproximately the same program) do not work.

--- cpp11/boost/sync-tcp-dt-client-call.py
+++ cpp11/boost/sync-tcp-dt-client-call.py
@@ -4,14 +4,24 @@ from multiprocessing import Pool
 def finish_callback():
     print "done"
 
+def finish_callback2():
+    print "done " + str(i)
+
 def main() :
     pool = Pool(processes=10)
     for i in range(20):
         #pool.apply_async(call, ["./sync-tcp-dt-client.exe", "10.58.10.224"], callback=finish_callback)
         print i
-        pool.apply_async(Popen,
-                         [["./sync-tcp-dt-client.exe", "10.58.10.224"], PIPE],
-                         callback=finish_callback)
+
+        branch = 1
+        if branch == 0:
+            pool.apply_async(Popen,
+                             [["./sync-tcp-dt-client.exe", "10.58.10.224"], PIPE],
+                             callback=finish_callback)
+        elif branch == 1:
+            pool.apply_async(Popen,
+                             [["./echo-client-tcp-blocking.exe", "10.58.10.224", "777"], PIPE],
+                             [finish_callback2, [i]])
         pass
 
     return 0
--- cpp11/distr-ip/main.cpp
+++ cpp11/distr-ip/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
+#include <vector>
 
 //using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
-}
+int PORT = 8123;
 
+
+
+//no kernel since that requires serialisation
 enum class OpDef {
     BW,
     Sharpen,
@@ -23,5 +23,32 @@ struct TaskDef {
 struct WorkBatchDef {
     int rangeStart;
     int rangeEnd;
-    std::vector
+    std::vector<TaskDef> &work;
 };
+
+struct ConnDef {
+    boost::asio::op::tcp::socket sock;
+};
+
+
+void mainLoop() {
+    //read work definition
+    
+    //init acceptor and spawn one worker per new connection
+    boost::asio::io_service io_service;
+    boost::asio::ip::tcp::acceptor acceptor(io_service,
+                                            boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), PORT));
+
+    bool stop = false;
+
+    while (!stop) {
+        
+    }
+}
+
+int main()
+{
+    std::cout << "Hello World!" << "\n";
+    mainLoop();
+    return 0;
+}
