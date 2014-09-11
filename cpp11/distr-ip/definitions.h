d70f275ceb430394761669d05506b8510da4c61a
Alex Danila <petrisoralexandru.d@bucwks0777.buc.gameloft.org>
2014-09-11 16:59:14 +0300

    Remove bool to simplify code.

--- cpp11/distr-ip/definitions.h
+++ cpp11/distr-ip/definitions.h
@@ -11,13 +11,18 @@ enum class TransmitType : uint8_t  {
     PartialImageData = 2
 };
 
+enum class CompressionType : uint8_t {
+    None = 0,
+    Zlib = 1
+};
+
 //what we're sending to the client
 #define S_OP sizeof(uint8_t)
 #define S_TRANSMIT sizeof(uint8_t)
-#define S_COMPRESSED sizeof(uint8_t)
+#define S_COMPRESSION sizeof(uint8_t)
 #define S_DIMENSION sizeof(uint32_t)
 #define S_DATASIZE sizeof(uint32_t)
-#define S_HEADER_CLIENTWORKDEF (S_OP + S_TRANSMIT + S_COMPRESSED + S_DIMENSION + S_DATASIZE)
+#define S_HEADER_CLIENTWORKDEF (S_OP + S_TRANSMIT + S_COMPRESSION + S_DIMENSION + S_DATASIZE)
 
 
 //TODO - try to use a bit less direct memory access
@@ -26,15 +31,7 @@ struct ClientWorkDef {
         uint32_t pos = 0;
         memcpy(&op, header+pos, S_OP); pos += S_OP;
         memcpy(&transmit, header+pos, S_TRANSMIT); pos += S_TRANSMIT;
-        
-        //
-        uint8_t auxBool;
-        memcpy(&auxBool, header+pos, S_COMPRESSED); pos += S_COMPRESSED;
-        if (auxBool == 0)
-            compressed = false;
-        else
-            compressed = true;
-
+        memcpy(&compression, header+pos, S_COMPRESSION); pos += S_COMPRESSION;
         memcpy(&w, header+pos, S_DIMENSION); pos += S_DIMENSION;
         memcpy(&h, header+pos, S_DIMENSION); pos += S_DIMENSION;
         memcpy(&dataSize, header+pos, S_DIMENSION); pos += S_DATASIZE;
@@ -44,11 +41,15 @@ struct ClientWorkDef {
         uint32_t pos = 0;
         memcpy(header+pos, &op, S_OP); pos += S_OP;
         memcpy(header+pos, &transmit, S_TRANSMIT); pos += S_TRANSMIT;
+        memcpy(header+pos, &compression, S_COMPRESSION); pos += S_COMPRESSION;
+        memcpy(header+pos, &w, S_DIMENSION); pos += S_DIMENSION;
+        memcpy(header+pos, &h, S_DIMENSION); pos += S_DIMENSION;
+        memcpy(header+pos, &dataSize, S_DATASIZE); pos += S_DATASIZE;
     }
 
     OpType op;
     TransmitType transmit;
-    bool compressed;
+    CompressionType compression;
     uint32_t w, h; //0 and irelevant if TransmitType is FullFile
     uint32_t dataSize; //4 Gib max file size
 };
