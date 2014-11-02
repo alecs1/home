541e35b8e434a7c5c6befc4c4e2fe277a778598f
Alex Dănilă <nuorama@gmail.com>
2014-11-03 01:03:08 +0200

    EOD.

--- haskell/algorith-vs-cpp/Conway-life/conway-array.hs
+++ haskell/algorith-vs-cpp/Conway-life/conway-array.hs
@@ -20,6 +20,25 @@ getNumbers contents = map strToDouble (words contents)
 strToDouble :: String -> Double
 strToDouble str = read str :: Double
 
-getNextGen ::
+-- first iteration, we suppose that the universe is bounded 0..n, 0..n
+getNextGen :: Int -> Array (Int, Int) -> Array (Int, Int)
+getNextGen inArray = outArray where
+                    outArray = array((1, 1), (n, n))
+                            ( [((i, j), 1) | neighCount inArray, i, j 
+
+
+--size, input array, x, y
+neighCount :: Int -> Array (Int, Int) -> Int -> Int -> Int
+neighCount 
+
+
+
+wavefront   :: Int -> Array(Int, Int) Int
+wavefront n = a where
+              a = array ((1,1), (n,n))
+                  ([((1,j), 1) | j <- [1..n]] ++
+                   [((i, 1), 1) | i <- [2..n]] ++
+                   [((i, j), a!(i,j-1) + a!(i-1,j-1) + a!(i-1,j)) | i <- [2..n], j <- [2..n]])
+
 
 
