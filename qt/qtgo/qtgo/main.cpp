db5ef59eb120bf62da6ca363fb3027172d75e021
user <user@debian>
2015-03-25 17:42:38 +0200

    Loaded QtCurve as shared library. Next: make it as static for Android.

--- qt/qtgo/qtgo/main.cpp
+++ qt/qtgo/qtgo/main.cpp
@@ -3,7 +3,10 @@
 
 #include "GameStruct.h"
 
-#include <QDir>
+//#include <QDir>
+#include <QPluginLoader>
+#include <QStylePlugin>
+#include <QStyle>
 
 //GameStruct game;
 
@@ -14,6 +17,27 @@ int main(int argc, char *argv[])
     //game.size = 19;
 
     QApplication a(argc, argv);
+
+    //dynamic loading on pc:
+    QPluginLoader loader("qtcurve.so", &a);
+    QObject* pluginInstance = loader.instance();
+    if (pluginInstance != NULL) {
+        printf("%s - loaded qtcurve.so\n", __func__);
+        QStylePlugin* qtCurveStylePlugin = qobject_cast<QStylePlugin*>(pluginInstance);
+        if (qtCurveStylePlugin != NULL) {
+            QStyle* qtCurve = qtCurveStylePlugin->create("QtCurve");
+            if (qtCurve != NULL) {
+                QApplication::setStyle(qtCurve);
+            }
+            else
+                printf("%s - style not created\n", __func__);
+        }
+        else
+            printf("%s - stylePlugin not loaded\n", __func__);
+    }
+    else
+        printf("%s - qtcurve.so file not loaded\n", __func__);
+
     MainWindow w;
     w.show();
 
