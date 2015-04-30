34dce5c7dbe4de466353e769f4096e79b1b1a16f
Alex Danila <petrisoralexandru.d@bucwks0777.buc.gameloft.org>
2015-04-30 19:16:42 +0300

    Just added everything.

---
+++ unity/UnityGo/Assets/Scripts/MouseManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseManager : MonoBehaviour {
+	public GameObject stone;
+
+	Ray camToMouseRay;
+	float maxDist = 1000f;
+
+	void Start() {
+	}
+
+	void Update() {
+		//Debug.Log(Time.frameCount);
+		//mouse or touch screen
+		if (Input.touches.Length > 0) {
+			for (int i = 0; i < Input.touches.Length; i++) {
+				Touch touch = Input.touches[i];
+				camToMouseRay = Camera.main.ScreenPointToRay(touch.position);
+			}
+		}
+		else {
+			camToMouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		}
+
+		//if the ray intersects the table, show a stone
+		RaycastHit tableHit;
+
+		int tableMask = LayerMask.GetMask("Table");
+		if (Physics.Raycast(camToMouseRay, out tableHit, maxDist, tableMask)) {
+			Vector3 newPos = tableHit.point;
+			newPos.y = 0;
+			stone.transform.position = newPos;
+		}
+		//Debug.Log (tableHit.ToString());
+
+		if (Input.GetButton("Fire1")) {
+			stone = (GameObject)Instantiate(stone, stone.transform.position, stone.transform.rotation);
+		}
+
+	}
+}
