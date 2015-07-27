4a78efba9ac0913f9c8f6367bddaeeede6882bee
alex <alex@vnycwks0113.nyc.gameloft.org>
2015-07-27 11:20:09 -0400

    EOD.

--- qt/qtgo/qtgo/network/ProtoJson.h
+++ qt/qtgo/qtgo/network/ProtoJson.h
@@ -1,6 +1,8 @@
 #ifndef PROTOJSON_H
 #define PROTOJSON_H
 
+#include <stdint.h>
+
 namespace ProtoJson {
 
 #define UUID_LEN 16
@@ -36,14 +38,34 @@ enum class ReplyType:uint8_t {
     Invalid = 0xFF
 };
 
-struct Msg {
-    MsgType mType = MsgType::Invalid;
-    uint msgid = 0x0;
+struct SReply {
+    ReplyType result = ReplyType::Invalid;
 };
 
-struct Command {
+struct SCommand {
     CmdType cType = CmdType::Invalid;
+    char uuid[UUID_LEN];
+    char isBlack;
+    uint8_t row = 0xFF;
+    uint8_t col = 0xFF;
+    uint8_t size = 0xFF;
+    float komi = -1000;
+    uint8_t handicap = 0xFF;
+    uint8_t handicapType = 0xFF;
 };
+
+
+struct SMsg {
+    SCommand* command = NULL;
+    SReply* reply = NULL;
+};
+
+struct Msg {
+    MsgType mType = MsgType::Invalid;
+    unsigned int msgid = 0x0;
+
+};
+
 
 
 }
