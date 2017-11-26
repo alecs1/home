d53994ba651d583a2ecf0ceb8979665ce19998dd
Alex Danila <alex.danila@gmail.com>
2017-11-26 14:49:20 +0200

    - unsaved stuff from home

--- qt/qtgo/freego/network/ConnMan.cpp
+++ qt/qtgo/freego/network/ConnMan.cpp
@@ -106,7 +106,7 @@ void ConnMan::processMessages() {
                     break;
                 }
                 case ConnState::Connected: {
-                    if (msg.msgType >= MsgType::CommonGames && msg.MsgType <= PlayMove) {
+                    if (msg.msgType >= MsgType::CommonGames && msg.msgType <= PlayMove) {
                         //forward the message
                     }
                 }
