22d7b2d254ee22277d85d3991145b9bb6647b20d
Alex Danila <alex.danila@gmail.com>
2017-12-17 14:21:57 +0200

    - fix crash

--- qt/qtgo/freego/Constants.h
+++ qt/qtgo/freego/Constants.h
@@ -15,12 +15,12 @@ namespace JsonKw {
     const QString PlayerType_AI = "AI";
     const QString PlayerType_LocalHuman = "LocalHuman";
     const QString PlayerType_Network = "Network";
+    const QString PlayerType_None = "None";
 }
 
 
 const boost::bimap<PlayerType, QString> playerTypeMap = boost::assign::list_of<boost::bimap<PlayerType, QString>::relation>
         (PlayerType::AI, JsonKw::PlayerType_AI)
         (PlayerType::LocalHuman, JsonKw::PlayerType_LocalHuman)
-        (PlayerType::Network, JsonKw::PlayerType_Network);
-
-
+        (PlayerType::Network, JsonKw::PlayerType_Network)
+        (PlayerType::None, JsonKw::PlayerType_None);
